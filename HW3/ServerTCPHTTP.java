f23b5449e57a5f8627752c071f0e75d06e8415ed
Alyssa Williams <38333607+chillyssa@users.noreply.github.com>
2023-02-17 21:21:42 -0700

    re adding Server file

--- HW3/ServerTCPHTTP.java
+++ HW3/ServerTCPHTTP.java
@@ -5,29 +5,30 @@
  * Description: HW03 - Main Server program
 */
 
-import java.net.*; 
-import java.io.*; 
+import java.net.*;
+import java.io.*;
 
-public class ServerTCPHTTP{
-    public static void main (String[] args) throws IOException{
-        ServerSocket serverTCPSocket = null; 
-        boolean listening = true; 
+public class ServerTCPHTTP {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverTCPSocket = null;
+        boolean listening = true;
         int port = 5310; // 5140 Jesse, 5310 Alyssa
 
         // Listen to the given port and wait for a connection request from a HTTP Client
-        try{
-            serverTCPSocket = new ServerSocket(port); 
+        try {
+            serverTCPSocket = new ServerSocket(port);
 
-        }catch(IOException e){
-            System.err.println("Could not listen on the specified port: " + port); 
-            System.exit(-1); 
+        } catch (IOException e) {
+            System.err.println("Could not listen on the specified port: " + port);
+            System.exit(-1);
         }
-        // Create a new thread for every incoming TCP connection request from a HTTP client
-        while(listening){
+        // Create a new thread for every incoming TCP connection request from a HTTP
+        // client
+        while (listening) {
             new TCPHTTPServerThread(serverTCPSocket.accept()).start();
         }
 
-        // Close server socket 
+        // Close server socket
         serverTCPSocket.close();
     }
 }
